fd453ee5d67bdb0a738645cc7de295cb3fc25d0d
ThisuraGallage <tggallage1@gmail.com>
2023-09-04 22:39:46 +0530

    backend added

---
+++ HotelMIS/src/test/java/com/example/HotelMIS/HotelMisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HotelMIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelMisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
